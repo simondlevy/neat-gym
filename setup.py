c9e22c458e5fd259c56fd93035d13cdda3eb8951
simondlevy <simon.d.levy@gmail.com>
2020-11-18 21:28:21 -0500

    Support distutils

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+Python distutils setup file for neat-gym package.
+
+Copyright (C) 2020 Simon D. Levy
+
+MIT License
+'''
+
+#from distutils.core import setup
+from setuptools import setup
+
+setup (name = 'neat_gym',
+    version = '0.1',
+    install_requires = ['gym', 'numpy'],
+    description = 'Use NEAT to learn OpenAI Gym environment',
+    packages = ['neat_gym'],
+    author='Simon D. Levy',
+    author_email='simon.d.levy@gmail.com',
+    url='https://github.com/simondlevy/gym-copter',
+    license='MIT',
+    platforms='Linux; Windows; OS X'
+    )
