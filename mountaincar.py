2f4e8e0780eee3f7efc0a86f57578b752d635070
simondlevy <simon.d.levy@gmail.com>
2020-12-03 02:23:20 -0500

    Cleanup

--- mountaincar.py
+++ mountaincar.py
@@ -3,40 +3,38 @@ import gym
 import neat 
 import numpy as np
 
-def run_neat(gens, env, max_steps, config, max_trials=100, output=True):
+def eval_fitness(genomes, config):
 
-    def eval_fitness(genomes, config):
+    env = gym.make('MountainCar-v0')
+    max_steps = 200
 
-        for idx, g in genomes:
-            net = neat.nn.FeedForwardNetwork.create(g, config)
+    for idx, g in genomes:
 
-            fitnesses = []
+        net = neat.nn.FeedForwardNetwork.create(g, config)
 
-            for i in range(1):
-                ob = env.reset()
+        fitnesses = []
 
-                total_reward = 0
+        for i in range(1):
+            ob = env.reset()
 
-                for j in range(max_steps):
-                    o = net.activate(ob)
-                    action = np.argmax(o)
-                    ob, reward, done, info = env.step(action)
-                    total_reward += reward
-                    if done:
-                        break
-                fitnesses.append(total_reward)
-            
-            g.fitness = np.array(fitnesses).mean()
+            total_reward = 0
 
-    pop = neat.population.Population(config)
-
-    pop.run(eval_fitness, gens)
+            for j in range(max_steps):
+                o = net.activate(ob)
+                action = np.argmax(o)
+                ob, reward, done, info = env.step(action)
+                total_reward += reward
+                if done:
+                    break
+            fitnesses.append(total_reward)
+        
+        g.fitness = np.array(fitnesses).mean()
 
 config = neat.config.Config(neat.genome.DefaultGenome, neat.reproduction.DefaultReproduction,
         neat.species.DefaultSpeciesSet, neat.stagnation.DefaultStagnation,
         'config_neat_mountain_car')
 
-env = gym.make('MountainCar-v0')
+pop = neat.population.Population(config)
 
-run_neat(1, env, 8, config, max_trials=0)
+pop.run(eval_fitness, 1)
 
