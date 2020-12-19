8e247beb39eefce9bba71108f3da5ea98c846f98
simondlevy <simon.d.levy@gmail.com>
2020-12-19 05:53:01 -0500

    New HyperNEAT working for CartPole-v1

--- hyperneat-evolve.py
+++ hyperneat-evolve.py
@@ -43,7 +43,7 @@ def main():
     # Parse command-line arguments
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--env', default='Pendulum-v0', help='Environment id')
+    parser.add_argument('--env', default='CartPole-v1', help='Environment id')
     parser.add_argument('--checkpoint', dest='checkpoint', action='store_true', help='Save at each new best')
     parser.add_argument('--cfgdir', required=False, default='./config', help='Directory for config files')
     parser.add_argument('--ngen', type=int, required=False, help='Number of generations to run')
@@ -62,10 +62,8 @@ def main():
     subs =  cfg['Substrate']
     actfun = subs['function']
     inp = eval(subs['input'])
+    hid = eval(subs['hidden'])
     out = eval(subs['output'])
-
-    NHIDS = (5,5) # XXX should come from config file
-    hid = [list(zip(np.linspace(-1,+1,n), [0.]*n)) for n in NHIDS]
 
     evalfun = _GymHyperConfig.eval_genome
     substrate = Substrate(inp, out, hid)
