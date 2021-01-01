816e46556a4892aa9051b753c195cbc1c1719b1c
simondlevy <simon.d.levy@gmail.com>
2021-01-01 11:27:35 -0500

    Begin re-merge of evolve scripts (Issue 26)

--- neat-evolve.py
+++ neat-evolve.py
@@ -7,6 +7,29 @@ Copyright (C) 2020 Simon D. Levy
 MIT License
 '''
 
-from neat_gym import _GymNeatConfig, _evolve_gym
+import argparse
+from argparse import ArgumentDefaultsHelpFormatter
+from neat_gym import _GymNeatConfig, evolve
 
-_evolve_gym(_GymNeatConfig.make_config)
+# Parse command-line arguments
+parser = argparse.ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+parser.add_argument('--env', default='CartPole-v1', help='Environment id')
+parser.add_argument('--checkpoint', dest='checkpoint', action='store_true',
+                    help='Save at each new best')
+parser.add_argument('--cfgdir', required=False,
+                    default='./config', help='Directory for config files')
+parser.add_argument('--ngen', type=int, required=False,
+                    help='Number of generations to run')
+parser.add_argument('--reps', type=int, default=10, required=False,
+                    help='Number of repetitions per genome')
+parser.add_argument('--seed', type=int, required=False,
+                    help='Seed for random number generator')
+args = parser.parse_args()
+
+# Get configuration and genome evaluation function for a particular algorithm
+configfun = _GymNeatConfig.make_config
+
+config, evalfun = configfun(args)
+
+# Evolve
+evolve(config, evalfun, args.seed, args.env, args.ngen, args.checkpoint)
